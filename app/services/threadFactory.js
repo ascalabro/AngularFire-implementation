706ec63f2d640bf9ec6429a41844c046cd617a6f
Angelo Scalabroni <armotek5421@msn.com>
2015-04-10 07:49:48 -0400

    functional create-delete and list of threads on spa

--- app/services/threadFactory.js
+++ app/services/threadFactory.js
@@ -1,12 +1,3 @@
 service.factory('threadFactory', function ( $resource, config) {
   return $resource(config.firebaseAppUrl + "/threads/:id.json");
 });
-//service.factory('threadFactory', function($resource, config) {
-//    return $resource(
-//            config.firebaseAppUrl + "/threads/:id.json",
-//            {}, // Query parameters
-//            {'query': {method: 'GET', isArray: false},
-//                'save': {method: 'POST', isArray: false}
-//            }
-//    );
-//})
